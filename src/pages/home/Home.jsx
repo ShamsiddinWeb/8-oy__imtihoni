ff0f45fa1cfd4d817cbaeec816fda62277a6049e
ShamsiddinWeb <shamsiddinnorov734@gmail.com>
2024-07-06 15:39:40 +0500

    imtihon

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
-import Catalogs from "../../components/catalogs/Catalogs";
 import Nornlight from "../../components/Nornlight/Nornlight";
 import Blog from "../../components/Blog/Blog";
 import Lamps from "../../components/lamps/Lamps";
@@ -8,10 +7,11 @@ import { useGetProductsQuery } from "../../context/api/productApi";
 import { catalog } from "../../data/catalogData/CatalogData";
 import Brands from "../../components/brands/Brands";
 import Product from "../../components/product/Product";
+import Catalogs from "../../components/catalogs/Catalogs";
 
 
 const Home = () => {
-  const { data, isLoading, isError, isSuccess } = useGetProductsQuery();
+  const { data, isLoading} = useGetProductsQuery();
   return (
     <div>
       <Hero />
